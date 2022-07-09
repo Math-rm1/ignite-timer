7c0d7362104baca79c5cd3ce44fc15c8a526f767
Math-rm1 <matheus.r.mariano@hotmail.com>
2022-07-09 09:46:49 -0300

    feat(router): add routes to pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
 import { GlobalStyled } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <Router />
       <GlobalStyled />
     </ThemeProvider>
   )
